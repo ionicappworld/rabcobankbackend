bac1111e0ba899418a4bf979311621b4cff1a63f
Vinesh <ionicappworld@gmail.com>
2018-07-08 08:01:45 -0700

    written failure case

--- src/test/java/org/rabcobank/report/business/CsvFileParserTest.java
+++ src/test/java/org/rabcobank/report/business/CsvFileParserTest.java
@@ -1,5 +1,6 @@
 package org.rabcobank.report.business;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -52,6 +53,32 @@ public class CsvFileParserTest {
 	}
 	
 	@Test
+	public void readCsvFileDataFail() {
+		CSVParser parser = null;
+
+		try {
+			ClassLoader classLoader = getClass().getClassLoader();
+			File file = new File(classLoader.getResource("records.csv").getFile());
+			CSVFormat csvFileFormat = CSVFormat.RFC4180.withHeader().withDelimiter(',');
+			parser = new CSVParser(new FileReader(file), csvFileFormat);
+			assertFalse(parser.getRecords().isEmpty());			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				parser.close();
+			} catch (IOException e) {
+				System.out.println("Error while closing fileReader/csvFileParser !!!");
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Test
 	public void writeValidtedReportCsv() {
 		
 		List<CustomerStatements> stataments = new ArrayList<CustomerStatements>();
@@ -80,6 +107,54 @@ public class CsvFileParserTest {
 				System.out.println("Sum of balance : " +(stats.getStart_Balance().add(stats.getMutation())));
 				System.out.println("Total : " +(stats.getStart_Balance().add(stats.getMutation())).compareTo(stats.getEnd_Balance()));
 				assertTrue(stats.getStart_Balance().add(stats.getMutation()).compareTo(stats.getEnd_Balance())== 0);
+
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fileWriter.flush();
+				fileWriter.close();
+				csvFilePrinter.close();
+			} catch (IOException e) {
+				System.out.println("Error while flushing/closing fileWriter/csvPrinter !!!");
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	@Test
+	public void writeValidtedReportCsvFail() {
+		
+		List<CustomerStatements> stataments = new ArrayList<CustomerStatements>();
+		CustomerStatements cs1 = new CustomerStatements(1234,"testing1","testing2",new BigDecimal(11),new BigDecimal(23),new BigDecimal(22));
+		CustomerStatements cs21 = new CustomerStatements(1234,"testing2","testing3",new BigDecimal(3),new BigDecimal(-1),new BigDecimal(6));
+		stataments.add(cs1);
+		stataments.add(cs21);
+
+		FileWriter fileWriter = null;
+		CSVPrinter csvFilePrinter = null;
+		CSVFormat csvFileFormat = CSVFormat.DEFAULT.withRecordSeparator("\n");
+		try {
+			fileWriter = new FileWriter("ValidatedCSVReports.csv");
+			csvFilePrinter = new CSVPrinter(fileWriter, csvFileFormat);
+			List<Integer> reference = new ArrayList<Integer>();
+
+			for (CustomerStatements stats : stataments) {
+				List<String> validatedStats = new ArrayList<String>();
+				validatedStats.add(String.valueOf(stats.getRefernece()));
+				validatedStats.add(stats.getAccountNumber());
+				validatedStats.add(stats.getDescription());
+				validatedStats.add(String.valueOf(stats.getStart_Balance()));
+				validatedStats.add(String.valueOf(stats.getMutation()));
+				validatedStats.add(String.valueOf(stats.getEnd_Balance()));			
+				reference.add(stats.getRefernece());	
+				System.out.println("Sum of balance : " +(stats.getStart_Balance().add(stats.getMutation())));
+				System.out.println("Total : " +(stats.getStart_Balance().add(stats.getMutation())).compareTo(stats.getEnd_Balance()));
+				assertFalse(stats.getStart_Balance().add(stats.getMutation()).compareTo(stats.getEnd_Balance())== 0);
 
 			}
 
